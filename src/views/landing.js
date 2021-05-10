ba18ce9ae2d5d8326a06a5e8d1c48ef7076e5f7b
SSaenzC <66050169+SSaenzC@users.noreply.github.com>
2021-05-10 13:36:14 -0500

    se agrega archivo css y se modifica el style de landing y sign up

--- src/views/landing.js
+++ src/views/landing.js
@@ -1,14 +1,17 @@
 export function landing() {
   const htmlLanding = `
-    <img src="../src/images/meowBoxMarcaBlanca.png" alt="">
-    <p>Personas con un gusto en común, los gatitos.</p>
-    <img src="../src/images/meowBoxCabeza.png" alt="">
-    <p>Haz parte de nuestra comunidad</p>
-    <h2> <a href="#/signUp">Crea tu cuenta fácil y rapido</a></h2>
-    <p>ó</p>
-    <h2><a href= "#/logIn">Ingresa con tu usuario</a></h2>`;
+    <div class="divLanding">
+      <img src="../src/images/meowBoxMarcaBlanca.png" alt="logo">
+      <p>Personas con un gusto en común, los gatitos.</p>
+      <img src="../src/images/meowBoxCabeza.png" alt="">
+      <p>Haz parte de nuestra comunidad</p>
+      <h2> <a href="#/signUp" style="color:#ffffff">Crea tu cuenta fácil y rapido</a></h2>
+      <p>ó</p>
+      <h2><a href= "#/logIn" style="color:#ffffff">Ingresa con tu usuario</a></h2>
+    </div>`;
 
   const landingView = document.createElement('section');
+  landingView.className = 'landingClass';
   landingView.innerHTML = htmlLanding;
   return landingView;
 }
--- src/views/signUp.js
+++ src/views/signUp.js
@@ -1,25 +1,28 @@
 export function signUp() {
   const signUpHtml = `
-    <img src="../src/images/MeowBoxMarcaMorada.png" alt="">
- <h2>Crea tu cuenta</h2>
- <button > <img src="../src/images/google.png" alt=""> Crear con Google</button>
- <p>ó</p>
- <section id="sectionformSignUp">
-  <form action="" id="formSignUp">
-    <label for="nameRegister">Nombre:</label>
-    <input type="text">
-    <label for="emailRegister">Correo:</label>
-    <input type="text" id="emailRegister">
-    <label for="passwordRegister">Contraseña:</label>
-    <input type="text" id="passwordRegister">
-    <input type="submit" value="resgistrarse">
-  </form>
+  <div class="divSignUp">
+    <img src="../src/images/MeowBoxMarcaMorada.png" alt="logo" width="50%" height="50%">
+    <h2>Crea tu cuenta</h2>
+    <button > <img src="../src/images/google.png" alt="googlelogo"width="10%" height="20%"> Crear con Google</button>
+    <p>ó</p>
+    <section id="formSignUp">
+    <form action="">
+      <h3>Nombre:</h3>
+      <input type="text">
+      <h3>Correo:</h3>
+      <input type="text">
+      <h3>Contraseña:</h3>
+      <input type="text">
+      <input type="submit" value="resgistrarse">
+    </form>
 
-  <h4>Ya tienes cuenta?</h4> <p id="getIn"><a href="#/logIn">Ingresa</a></p> 
+    <h4>Ya tienes cuenta?</h4> <p id="getIn"><a href="#/logIn">Ingresa</a></p> 
   
- </section>`;
+    </section>
+  </div>`;
 
-  const SignUpView = document.createElement('section');
-  SignUpView.innerHTML = signUpHtml;
-  return SignUpView;
+  const signUpView = document.createElement('section');
+  signUpView.className = 'signUpClass';
+  signUpView.innerHTML = signUpHtml;
+  return signUpView;
 }
